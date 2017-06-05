6fc6e9510b708aab77a4b4a8c514917afda2336e
loyd0 <samloyd@hotmail.co.uk>
2017-06-05 22:38:08 +0100

    added all the word lists

---
+++ lib/word_lists/pronouns.rb
@@ -0,0 +1,3 @@
+self_array = ["i", "i'm", "me", "mine", "my", "myself", "oneself"]
+our = ["let's", "out", "ours", "ourselves", "us", "we"]
+you = ["thee", "thou", "thy", "you", "your", "yours", "yourself", "yourselves"]
